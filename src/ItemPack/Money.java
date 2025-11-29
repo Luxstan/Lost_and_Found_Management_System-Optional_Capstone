7a4202963f1c19d1b7d671c90eafe6f06fe26a4b
PoorCoconut <cueshealforque@gmail.com>
2025-11-30 01:16:49 +0800

    created the system for console use. I added extra methods and fields for the classes: Item, LFSystem, Money, User
    I also disabled the Form in the main. [gi comment out ra nko]

--- src/ItemPack/Money.java
+++ src/ItemPack/Money.java
@@ -3,4 +3,19 @@ package ItemPack;
 public class Money extends Item{
     private double amount;
     private boolean inWallet;
+
+    public Money(double amount, boolean inWallet){
+        super("Money");
+        this.amount = amount;
+        this.inWallet = inWallet;
+    }
+
+    //Setters and Getters
+    public double getAmount(){
+        return amount;
+    }
+
+    public boolean getInWallet(){
+        return inWallet;
+    }
 }
