459dcd484ef81351d873e24bce4fdb35f6f9bcb1
LAB15_USER ON WS10 <L15X15W10@LabHCI.net>
2025-12-01 17:52:01 +0800

    Main Menu GUI Form

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,44 +1,6 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class MainMenu extends JFrame{
-    private JButton ReportMissingItemBtn;
-    private JButton FoundItemBtn;
-    private JButton ExitBtn;
-    private JLabel MainMenu;
-    private JPanel JPanel;
-
-
-    public MainMenu(){
-        setContentPane(JPanel);
-        setTitle("Lost and Found Management System");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800,600);
-        setLocationRelativeTo(null);
-
-        ReportMissingItemBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(MainMenu.this, "Report Missing Item");
-            }
-        });
-        FoundItemBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(MainMenu.this, "Click");
-            }
-        });
-        ExitBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+public class MainMenu {
+    private JButton button2;
+    private JButton button3;
 }
